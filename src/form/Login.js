aca953748b78446ad3140a1181f3b5431df1eaa4
Vivek Viradiya <vivekviradiya8@gmail.com>
2024-08-01 17:46:38 +0530

    css file name change

--- src/form/Login.js
+++ src/form/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
-import "./auth.css";
+import "../form/style.css";
 
 const Login = () => {
   const [username, setUsername] = useState("");
--- src/form/Signup.js
+++ src/form/Signup.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
-import "./auth.css";
+import "../form/style.css";
 
 const Signup = () => {
   const navigate = useNavigate();
