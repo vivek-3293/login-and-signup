81a0dd64ac8301043ee8d9b8aab5d68e87743714
Vivek Viradiya <vivekviradiya8@gmail.com>
2024-08-02 16:12:33 +0530

    added

--- src/form/Login.js
+++ src/form/Login.js
@@ -14,8 +14,8 @@ const Login = () => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        username: username,
-        password: password,
+        username,
+        password,
       })
     })
     .then(res => res.json())
